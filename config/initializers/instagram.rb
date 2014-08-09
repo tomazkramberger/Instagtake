09a74ad1fd663b6765d32352d97b9bc415c1751a
Tomaz Kramberger <tomazkramberger@gmail.com>
2014-08-09 09:06:36 +0200

    Added Instagram production config

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,7 +3,7 @@ require "instagram"
 Instagram.configure do |config|
     
     if Rails.env.production?
-    	config.client_id = "8b783dca69c047a7884c4f01e01d855c"
+		config.client_id = "8b783dca69c047a7884c4f01e01d855c"
 	  	config.access_token = "33085202.8b783dc.beb4e72994784a9c828272dd6cf593d3"
     else
 		config.client_id = "8db2485ad3954da585d59829937b3a2e"
